dcc3e9f5e10d419e5d9aca58516b496f3c39aa17
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-06-11 13:05:48 +0800

    modified endpoints

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/salesOrders', [SalesOrderController::class, 'index']);
-Route::get('/salesOrders/{salesOrder}', [SalesOrderController::class, 'show']);
 Route::middleware('auth:sanctum')->resource('salesOrder', SalesOrderController::class, [
-    'except' => ['create', 'edit', 'show', 'index']
+    'except' => ['create', 'edit']
 ]);
