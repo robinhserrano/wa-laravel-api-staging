d93a9a5a149efe20a7fd3ec920c46597256d713d
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-06-12 17:22:57 +0800

    Updated salesOrder store function, can now add OrderLines and update an existing sales order

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -6,6 +6,7 @@ use App\Models\OrderLine;
 use App\Models\SalesOrder;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class SalesOrderController extends Controller
 {
@@ -31,13 +32,41 @@ class SalesOrderController extends Controller
      */
     public function store(Request $request)
     {
-        try {
-            SalesOrder::create($request);
-            return response()->json(['message' => 'Sales order created successfully'], 201);
-        } catch (\Exception $e) {
-            // Handle validation errors or other exceptions
-            return response()->json(['error' => $e->getMessage()], 422); // Unprocessable Entity
+        $orderData = $request->all(); // Get all request data as an array
+
+        // Check if a SalesOrder with the same name already exists
+        $existingOrder = SalesOrder::where('name', $orderData['name'])->first();
+        $allowedColumns = ['amount_to_invoice', 'amount_total', 'amount_untaxed', 'create_date', 'delivery_status', 'internal_note_display', 'name', 'partner_id_contact_address', 'partner_id_display_name', 'partner_id_phone', 'state', 'x_studio_commission_paid', 'x_studio_invoice_payment_status', 'x_studio_payment_type', 'x_studio_referrer_processed', 'x_studio_sales_rep_1', 'x_studio_sales_source'];
+        $allowedColumns2 = ['sales_order_id', 'product', 'description', 'quantity', 'unit_price', 'tax_excl', 'disc', 'taxes', 'delivered', 'invoiced'];
+
+        if ($existingOrder) {
+            // Name already exists, handle update scenario
+            $existingOrder->update(Arr::only($orderData, $allowedColumns));
+            return response()->json(['message' => 'Sales order updated successfully'], 200); // OK
+        } else {
+            // New Sales Order, create a new instance
+
+            $salesOrder = new SalesOrder();
+            $salesOrder->fill(Arr::only($orderData, $allowedColumns));
+            $salesOrder->save();
+
+            foreach ($request['order_line'] as $orderLineData) {
+                // Set the sales_order_id based on the parent SalesOrder
+                $orderLineData['sales_order_id'] = $salesOrder->id;
+
+                // Create and save a new OrderLine instance
+                OrderLine::create(Arr::only($orderLineData, $allowedColumns2));
+            }
         }
+
+        // foreach ($request['order_line'] as $orderLineData) {
+        //     // Set the sales_order_id based on the parent SalesOrder
+        //     $orderLineData['sales_order_id'] = $salesOrder->id;
+
+        //     // Create and save a new OrderLine instance
+        //     OrderLine::create($orderLineData);
+        // }
+        return response()->json(['message' => 'Sales order created successfully'], 201); // Created
     }
 
     /**
@@ -62,22 +91,22 @@ class SalesOrderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $validatedData = $request->validate([
-            'sales_order_id' => 'required|exists:sales_orders,id',
-            'product' => 'required',
-            'quantity' => 'required|integer|min:1',
-            'unit_price' => 'required|numeric|min:0',
-            'tax_excl' => 'required|numeric|min:0',
-            'disc' => 'required|numeric|min:0',
-            'delivered' => 'required|boolean',
-            'invoiced' => 'required|boolean',
-            // Add validation rules for other fields
-        ]);
+        // $validatedData = $request->validate([
+        //     'sales_order_id' => 'required|exists:sales_orders,id',
+        //     'product' => 'required',
+        //     'quantity' => 'required|integer|min:1',
+        //     'unit_price' => 'required|numeric|min:0',
+        //     'tax_excl' => 'required|numeric|min:0',
+        //     'disc' => 'required|numeric|min:0',
+        //     'delivered' => 'required|boolean',
+        //     'invoiced' => 'required|boolean',
+        //     // Add validation rules for other fields
+        // ]);
 
-        $salesOrder = SalesOrder::findOrFail($id);
-        $salesOrder->update($validatedData);
+        // $salesOrder = SalesOrder::findOrFail($id);
+        // $salesOrder->update($validatedData);
 
-        return response()->json(['message' => 'Order line updated successfully'], 200);
+        // return response()->json(['message' => 'Order line updated successfully'], 200);
     }
 
     /**
